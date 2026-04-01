9df210930a1856c4ebd81c805f27f94a5bff0cf8
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-01 21:46:16 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/nodeapp99repo/Nodeapp99Application.java
@@ -0,0 +1,13 @@
+package com.example.nodeapp99repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nodeapp99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nodeapp99Application.class, args);
+	}
+
+}
